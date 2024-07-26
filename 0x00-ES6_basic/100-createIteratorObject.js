0deaaee35ac12072657b39bdbc5041109cce029c
math198512 <thoraichi@gmail.com>
2024-07-26 20:18:46 +0100

    100-createIteratorObject.js

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,6 +1,8 @@
 export default function createIteratorObject(report) {
   const employees = report.allEmployees;
   const reportWithIterator = [];
+
+  // eslint-disable-next-line guard-for-in
   for (const elem in employees) {
     for (const i of employees[elem]) {
       reportWithIterator.push(i);
