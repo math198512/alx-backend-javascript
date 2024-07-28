ef98bde44efffdc7d025baf2ee18711eb7fb037a
math198512 <thoraichi@gmail.com>
2024-07-28 19:01:22 +0100

    3-all.js

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -6,5 +6,5 @@ export default function handleProfileSignup() {
 
   return Promise.all([uploadPhotoPromise, createUserPromise]).then((results) => {
     console.log(`${results[0].body} ${results[1].firstName} ${results[1].lastName}`);
-  }).catch(() => console.log("Signup system offline"));
+  }).catch(() => console.log('Signup system offline'));
 }
