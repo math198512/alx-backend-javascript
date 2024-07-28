9034e38e773375a655a228e730276d947e70d90d
math198512 <thoraichi@gmail.com>
2024-07-28 18:56:25 +0100

    3-all.js

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,10 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  const uploadPhotoPromise = uploadPhoto();
+  const createUserPromise = createUser();
+
+  Promise.all([uploadPhotoPromise, createUserPromise]).then((results) => {
+    console.log(`${results[0].body} ${results[1].firstName} ${results[1].lastName}`);
+  });
+}
