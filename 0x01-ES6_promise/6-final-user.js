10ba8d8e50a8536797db12074652e274f7424fb8
math198512 <thoraichi@gmail.com>
2024-07-29 18:23:47 +0100

    6-final-user.js

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,16 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  const results = [];
+  try {
+    const user = await signUpUser(firstName, lastName);
+    results.push({ status: 'fulfilled', value: user });
+    await uploadPhoto(fileName);
+  } catch (err) {
+    results.push({
+      status: 'rejected',
+      value: `Error: ${fileName} cannot be processed`,
+    });
+  }
+  return results;
+}
