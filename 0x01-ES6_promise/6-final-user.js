f8db12d08b5ddf0c7581471897ed79f414daa4a4
math198512 <thoraichi@gmail.com>
2024-07-29 18:25:24 +0100

    6-final-user.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,5 +1,7 @@
 import signUpUser from './4-user-promise';
+
 import uploadPhoto from './5-photo-reject';
+
 export default async function handleProfileSignup(firstName, lastName, fileName) {
   const results = [];
   try {
