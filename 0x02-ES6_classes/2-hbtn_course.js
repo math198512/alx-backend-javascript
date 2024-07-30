6d2ebebcbb7b06d367b53dd5e86718a05bf9c1a8
math198512 <thoraichi@gmail.com>
2024-07-30 21:06:22 +0100

    2-hbtn_course.js

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,44 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set name(newName) {
+    if (typeof newName === 'string') {
+      this._name = newName;
+    } else {
+      throw new TypeError('Name must be a string');
+    }
+  }
+
+  set length(newLength) {
+    if (typeof newLength === 'number') {
+      this._length = newLength;
+    } else {
+      throw new TypeError('Length must be a number');
+    }
+  }
+
+  set students(newStudents) {
+    if (!Array.isArray(newStudents)) { throw new TypeError('Students must be an array'); }
+    // eslint-disable-next-line no-plusplus
+    for (let i = 0; i < newStudents.length; i++) {
+      if (typeof newStudents[i] !== 'string') { throw new TypeError('Students must be an array of strings'); }
+    }
+    this._students = newStudents;
+  }
+}
