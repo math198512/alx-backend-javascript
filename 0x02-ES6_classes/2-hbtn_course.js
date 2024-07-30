2da1de4e8aea2855bf30ad0eeead249136712a09
math198512 <thoraichi@gmail.com>
2024-07-30 21:46:41 +0100

    2-hbtn_course.js

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -21,7 +21,7 @@ export default class HolbertonCourse {
   }
 
   set length(length) {
-    if ((typeof length !== 'number') && !(length instanceof Number)) {
+    if (typeof length !== 'number') {
       throw new TypeError('Length must be a number');
     }
     this._length = length;
