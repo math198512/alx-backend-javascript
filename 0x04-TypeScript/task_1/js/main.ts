cb84af3fa8e75cb178d12dce4b60458e367626a2
math198512 <thoraichi@gmail.com>
2024-08-01 18:46:59 +0100

    main.ts

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,10 +1,10 @@
-interface Teacher {
+export interface Teacher {
     readonly firstName: string,
     readonly lastName: string,
     fullTimeEmployee: boolean,
     yearsOfExperience?: number,
     location: string,
-
+    [key: string]: any
 }
 
 interface Directors extends Teacher {
