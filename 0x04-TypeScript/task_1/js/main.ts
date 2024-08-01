89d469716fc2147e6ad939016195c3a6dcfe67b1
math198512 <thoraichi@gmail.com>
2024-08-01 18:51:54 +0100

    main.ts

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -10,3 +10,9 @@ export interface Teacher {
 export interface Directors extends Teacher {
     numberOfReports: number,
 }
+
+interface printTeacherFunction {
+    (firstName: string, lastName: string): string;
+}
+
+const printTeacher: printTeacherFunction = (firstName: string, lastName: string): string => `${firstName.charAt(0)}. ${lastName}`;
