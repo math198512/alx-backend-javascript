2c00c0539c4c19e0a2574963ccb40654486d47c1
math198512 <thoraichi@gmail.com>
2024-08-02 20:08:41 +0100

    3-get_ids_sum.js

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,6 +1,6 @@
 import getListStudents from './0-get_list_students';
 
-export default function getStudentsByLocation(students = getListStudents(), city) {
+export default function getStudentsByLocation(students = getListStudents()) {
   const initialValue = 0;
   const sum = students.reduce(
     (accumulator, student) => accumulator + student.id,
