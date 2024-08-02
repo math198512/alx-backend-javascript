8d1833cee9a9e0e45fbf34261b3b8145b5311547
math198512 <thoraichi@gmail.com>
2024-08-02 19:42:58 +0100

    2-get_students_by_loc.js

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,6 +1,5 @@
-import getListStudents from './0-get_list_students'
-
+import getListStudents from './0-get_list_students';
 
 export default function getStudentsByLocation(students = getListStudents(), city) {
-  return students.filter((student) => student.location === city)
+  return students.filter((student) => student.location === city);
 }
