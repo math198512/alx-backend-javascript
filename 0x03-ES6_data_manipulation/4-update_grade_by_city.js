a209daf49c04fb7b0906ee1e05aceca9dd11409b
math198512 <thoraichi@gmail.com>
2024-08-03 19:45:07 +0100

    4-update_grade_by_city.js

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,13 @@
+import getListStudents from './0-get_list_students';
+
+export default function updateStudentGradeByCity(students = getListStudents(), city, newGrades) {
+    return students.filter((student) => student.location === city)
+        .map((student) => {
+            const grades = newGrades.filter((el) => el.studentId === student.id);
+            const grade = grades.length ? grades[0].grade : 'N/A';
+            return {
+                ...student,
+                grade,
+            };
+        });
+}
