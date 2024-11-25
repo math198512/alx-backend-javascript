3df579d8dda433bca59154e3749f58d65e2bcc95
math198512 <thoraichi@gmail.com>
2024-11-25 13:51:28 +0100

    1-stdin.js

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,21 @@
+/**
+ * Displayq the message 'Welcome to Holberton School, what is your name?' (followed by a new line)
+ * The user should be able to input their name on a new line
+ * The program should display Your name is: INPUT
+ * When the user ends the program, it should display 'This important software is now closing' (followed by a new line)
+ * @author Tarik Horaichi <https://github.com/math198512>
+ */
+const process = require('process');
+
+console.log('Welcome to Holberton School, what is your name?');
+
+process.stdin.setEncoding('utf-8');
+
+process.stdin.on('data', (data) => {
+    const name = data.trim();
+    console.log(`Your name is: ${name}`);
+});
+process.on('SIGINT', () => {
+    console.log('This important software is now closing');
+    process.exit()
+});
