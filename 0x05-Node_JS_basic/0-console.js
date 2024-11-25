a8bc9520fb49f4be6c4c7063ffba0e294a0462af
math198512 <thoraichi@gmail.com>
2024-11-25 13:26:45 +0100

    0-console.js

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ * @author Tarik Horaichi <https://github.com/math198512>
+ */
+const displayMessage = (msg) => {
+    console.log(msg);
+};
+
+module.exports = displayMessage;
