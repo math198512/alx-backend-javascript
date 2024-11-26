2f150235c751cd6565798c5d74684544b4507fe4
math198512 <thoraichi@gmail.com>
2024-11-26 17:39:27 +0100

    1-stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -14,6 +14,6 @@ process.stdin.on('data', (data) => {
   const name = data.trim();
   console.log(`Your name is: ${name}`);
 });
-process.stdin.on('exit', () => {
-  process.stdout.write('This important software is now closing');
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
