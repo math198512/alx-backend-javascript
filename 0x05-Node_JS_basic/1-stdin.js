a2076f1046b0a6e32d6d8a705a7beccb18e74a50
math198512 <thoraichi@gmail.com>
2024-11-26 18:03:15 +0100

    1-stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -3,8 +3,8 @@ console.log('Welcome to Holberton School, what is your name?');
 process.stdin.setEncoding('utf-8');
 
 process.stdin.on('data', (data) => {
-  const name = process.stdin.read();
-  console.log(`Your name is: ${name}`);
+  const name = data.trim();
+  process.stdout.write(`Your name is: ${name}\n`);
 });
 process.stdin.on('end', () => {
   process.stdout.write('This important software is now closing\n');
