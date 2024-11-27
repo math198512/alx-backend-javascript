04e9596149b6e5524eed4679c6b5cec4014d147e
math198512 <thoraichi@gmail.com>
2024-11-27 22:14:15 +0100

    2-read_file.js

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,13 +1,13 @@
 const fs = require('fs');
 
 const countStudents = (path) => {
-  const fileLines = fs.readFileSync(`${path}`, 'utf-8').toString().split('\n');
   if (!fs.existsSync(path)) {
     throw new Error('Cannot load the database');
   }
   if (!fs.statSync(path).isFile()) {
     throw new Error('Cannot load the database');
   }
+  const fileLines = fs.readFileSync(`${path}`, 'utf-8').toString().split('\n');
   return fileLines;
 };
 module.exports = countStudents;
