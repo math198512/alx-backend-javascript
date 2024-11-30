564f8cd1db3ab363326bcd25df5ebbf7e82d974b
math198512 <thoraichi@gmail.com>
2024-11-30 10:53:07 +0100

    5-http.js

---
+++ 0x05-Node_JS_basic/5-http.js
@@ -0,0 +1,19 @@
+const http = require('http');
+
+// Create an HTTP server that responds to all requests with the same message
+const app = http.createServer((req, res) => {
+  // Set the response header to plain text
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  if (req.url === '/') {
+    // Send the response body
+    res.end('Hello Holberton School!');
+  }
+});
+
+// Make the server listen on port 1245
+app.listen(1245, () => {
+  console.log('Server running on port 1245');
+});
+
+// Export the app for potential use in other modules
+module.exports = app;
