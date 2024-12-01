968a781837bef1bb7eb1e7e6684a24a5b270ced0
math198512 <thoraichi@gmail.com>
2024-12-01 19:03:00 +0100

    4-payment.test.js

---
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -0,0 +1,21 @@
+const sinon = require('sinon');
+const Utils = require('./utils');
+const { expect } = require('chai');
+const sendPaymentRequestToApi = require('./3-payment');
+
+describe('sendPaymentRequestToApi', () => {
+  it('should call Utils.calculateNumber with the correct arguments', () => {
+    const bigBrother = sinon.spy(console);
+    const calculateNumberSpy = sinon.spy(Utils);
+    const myFunctionStub = sinon.stub(Utils, 'calculateNumber');
+    myFunctionStub.returns(10);
+    sendPaymentRequestToApi(100, 20);
+
+    expect(myFunctionStub.calledWith('SUM', 100, 20)).to.be.true;
+    expect(myFunctionStub.callCount).to.be.equal(1);
+    expect(bigBrother.log.calledWith('The total is: 10')).to.be.true;
+    expect(bigBrother.log.callCount).to.be.equal(1);
+    myFunctionStub.restore();
+    bigBrother.log.restore();
+  });
+});
